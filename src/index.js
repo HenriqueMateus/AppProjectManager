a8fddd367550760a42d5c609084079a72062ad3c
matolds <henrimateusoll@gmail.com>
2020-10-25 14:17:23 -0300

    adicionando lacia barraapp

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Rotas from './config/rotas/Routes'
-
+import AppBar from './components/Appbar'
 
 ReactDOM.render(
-  <Rotas/>,
+  <React.StrictMode>
+    <AppBar />
+    <Rotas />
+  </React.StrictMode>,
   document.getElementById('root')
 );
