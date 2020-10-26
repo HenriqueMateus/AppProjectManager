dcc59bddd02b50a3c05cc515cb7eff5fa1803a07
matolds <henrimateusoll@gmail.com>
2020-10-26 02:08:33 -0300

    adicionando cards estrutura

--- src/config/rotas/Routes.js
+++ src/config/rotas/Routes.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from '../../App.js'
 import Projetos from '../../pages/Projetos'
+import WorkProjeto from '../../pages/WorkProjeto'
 
 function Rotas() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact={true} component={App} />
-                <Route path="/projetos" component={Projetos} />
+                <Route path="/" exact component={App} />
+                <Route path="/Projetos" exact component={Projetos} />
+                <Route path="/Projetos/Orgprojeto" exact component={WorkProjeto} />
             </Switch>
         </ BrowserRouter>
     )
---
+++ src/pages/WorkProjeto/index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './style.css'
+import iconAdd from '../../Icon/icon-content-add_24px.png'
+export default function WorkProjeto() {
+    return (
+        <>
+            <div className='projetobarr'>
+                <div className='esquerdaBarr'>
+                    <h4>Em Aprovação</h4>
+                </div>
+                <div className='centroBarr'>
+                    <h4>Pesquisador: Rachel </h4>
+                    <button>Membros</button>
+                </div>
+                <div className='direitaBarr'>
+                    <h4>Inicio: 10/05/2020 </h4>
+                    <h4> Fim: </h4>
+                </div>
+            </div>
+            <div className='cards'>
+                <div className='card' id='afazer'>
+                    <h5>A fazer</h5>
+                    <div className='scroll'>
+                    <div className='cardTask'>
+                        <div className='cardColor' style={{ background: 'green' }} />
+
+                        <h6>Exemplo tarefa</h6>
+                    </div>
+                    <div className='cardTask'>
+                        <div className='cardColor' style={{ background: 'green' }} />
+
+                        <h6>Exemplo tarefa</h6>
+                    </div>
+                    <div className='cardTask'>
+                        <div className='cardColor' style={{ background: 'green' }} />
+
+                        <h6>Exemplo tarefa</h6>
+                    </div>
+                    <div className='cardTask'>
+                        <div className='cardColor' style={{ background: 'green' }} />
+
+                        <h6>Exemplo tarefa</h6>
+                    </div>
+                    <div className='cardTask'>
+                        <div className='cardColor' style={{ background: 'green' }} />
+
+                        <h6>Exemplo tarefa</h6>
+                    </div>
+                    <div className='cardTask'>
+                        <div className='cardColor' style={{ background: 'green' }} />
+
+                        <h6>Exemplo tarefa</h6>
+                    </div>
+                    </div>
+                    <div className='adicionarCard'>
+                        <button><img src={iconAdd}/>Adicionar novo card</button>
+                    </div>
+                </div>
+                <div className='card' id='andamento'>
+                    <h5>Em Andamento</h5>
+                    <div className='cardTask'>
+                        <div className='cardColor' style={{ background: 'green' }} />
+
+                        <h6>Exemplo tarefa</h6>
+                    </div>
+                </div>
+                <div className='card' id='concluido'>
+                    <h5>Concluido</h5>
+                    
+                </div>
+            </div>
+        </>
+
+    )
+}
