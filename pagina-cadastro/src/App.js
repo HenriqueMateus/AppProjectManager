1cc0b55b12f37f13ed7157e06d50d0a54768c1aa
Daniel2023 <55217179+Daniel2023@users.noreply.github.com>
2020-10-26 15:35:49 -0300

    update

--- pagina-cadastro/src/App.js
+++ pagina-cadastro/src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+
+<div className="rows">
+  <h1>Cadastro</h1>
+    <div className='row'>Nome</div>
+    <input type="text" />
+    <div className='row'>SobreNome</div>
+    <input type="text"  />
+    <div className='row'>Senha</div>
+    <input type="text"  />
+    <div className='row'>Confirme sua Senha</div>
+    <input type="text"  />
+    <div className='row'>Email</div>
+    <input type="text"  />
+    <div className='row'>Link Curriculum</div>
+    <input type="text" />
+</div>
   );
 }
 
 export default App;
+
