f3d5d1c02965763786cbe0f1de741c9b5e4da3bc
matolds <henrimateusoll@gmail.com>
2020-11-04 19:12:18 -0300

    add barra app

--- src/pages/Projetos/index.js
+++ src/pages/Projetos/index.js
@@ -1,74 +1,99 @@
-import React from 'react'
-import { ViewList } from '@material-ui/icons';
+import React, {useState} from 'react'
+import { Done, ViewList, Input } from '@material-ui/icons';
 import './style.css'
 import { Link } from 'react-router-dom';
 export default function Projetos(params) {
-    // const [valor, setValor] = useState(0)
+    const [openBar, setOpenBar] = useState(false)
     // const [list, setList] = useState([])
     // const scrollDireta = (id) => {
     //     let scrollId = document.querySelector(id)
 
     // }
-
+    const viewBar = () =>{
+        if(openBar){
+            setOpenBar(false)
+        } else {
+            setOpenBar(true)
+        }
+    }
     return (
         <div>
-            <div className='appbar'><ViewList onClick={() => console.log('ok')} />
+            <div className='appbar'><ViewList style={{ marginRight: 'auto', marginLeft: '10px' }} onClick={() => viewBar()} />
                 <h2> Projetos</h2>
             </div>
-            <div className='barList'></div>
-            <div className='blocProjetos' id='pendentes'>
-                <h2>Pendentes</h2>
-                <div className='scrollProjetos'>
-                    <Link to='/Projetos/Orgprojeto/3'>
-                        <div className='cardProjeto'>
-                            <div className='headerProjeto'><h3>Projeto TAL</h3></div>
-                            <div className='bodyProjeto'><p>A vida de redator freelancer não é fácil. Entre se manter atualizado em Marketing de Conteúdo e buscar novos clientes, também temos de dar muita atenção a uma habilidade básica: a escrita.</p>
-                                <p><strong>Criado por:</strong>Mateus Henrique de Oliveira</p></div>
-                            <div className='overProjeto'>
+            <div className='bodyIncial'>
+                { openBar &&
+                    <div className='barList'>
+                        <ul>
+                            <li>Criar Projeto</li>
+                            <li>Criar Projeto</li>
+                        </ul>
+                    </div>
+                }
+                <div className='projetosCategorias'>
+                    <div className='blocProjetos' id='pendentes'>
+                        <h2>Pendentes</h2>
+                        <div className='scrollProjetos'>
 
+                            <div className='cardProjeto'>
+                                <Link to='/Projetos/Orgprojeto/3'>
+                                    <div className='headerProjeto'><h3>Projeto TAL</h3></div>
+                                    <div className='bodyProjeto'><p>A vida de redator freelancer não é fácil. Entre se manter atualizado em Marketing de Conteúdo e buscar novos clientes, também temos de dar muita atenção a uma habilidade básica: a escrita.</p>
+                                        <p><strong>Criado por:</strong>Mateus Henrique de Oliveira</p></div>
+                                </Link>
+                                <div className='overProjeto'>
+                                    <button><Done />Aprovar</button>
+                                    <button><Input />Ingressar</button>
+                                </div>
                             </div>
+
+
+
                         </div>
-                    </Link>
 
 
+                    </div>
+                    <div className='blocProjetos'>
+                        <h2>Em andamento</h2>
+                        <div className='scrollProjetos'>
+
+                            <div className='cardProjeto'>
+                                <Link to='/Projetos/Orgprojeto/1'>
+                                    <div className='headerProjeto'><h3>Projeto TAL</h3></div>
+                                    <div className='bodyProjeto'><p>A vida de redator freelancer não é fácil. Entre se manter atualizado em Marketing de Conteúdo e buscar novos clientes, também temos de dar muita atenção a uma habilidade básica: a escrita.</p>
+                                        <p><strong>Criado por:</strong>Mateus Henrique de Oliveira</p></div>
+                                </Link>
+                                <div className='overProjeto'>
+
+                                </div>
+                            </div>
+
+
+
+
+                        </div>
+                    </div>
+                    <div className='blocProjetos'>
+                        <h2>Concluídos</h2>
+                        <div className='scrollProjetos'>
+
+                            <div className='cardProjeto'>
+                                <Link to='/Projetos/Orgprojeto/2'>
+                                    <div className='headerProjeto'><h3>Projeto TAL</h3></div>
+                                    <div className='bodyProjeto'><p>A vida de redator freelancer não é fácil. Entre se manter atualizado em Marketing de Conteúdo e buscar novos clientes, também temos de dar muita atenção a uma habilidade básica: a escrita.</p>
+                                        <p><strong>Criado por:</strong>Mateus Henrique de Oliveira</p></div>
+                                </Link>
+                                <div className='overProjeto'>
+
+                                </div>
+                            </div>
+
+
+
+                        </div>
+                    </div>
                 </div>
 
-
-            </div>
-            <div className='blocProjetos'>
-                <h2>Em andamento</h2>
-                <div className='scrollProjetos'>
-                    <Link to='/Projetos/Orgprojeto/1'>
-                        <div className='cardProjeto'>
-                            <div className='headerProjeto'><h3>Projeto TAL</h3></div>
-                            <div className='bodyProjeto'><p>A vida de redator freelancer não é fácil. Entre se manter atualizado em Marketing de Conteúdo e buscar novos clientes, também temos de dar muita atenção a uma habilidade básica: a escrita.</p>
-                                <p><strong>Criado por:</strong>Mateus Henrique de Oliveira</p></div>
-                            <div className='overProjeto'>
-
-                            </div>
-                        </div>
-                    </Link>
-
-
-
-                </div>
-            </div>
-            <div className='blocProjetos'>
-                <h2>Concluídos</h2>
-                <div className='scrollProjetos'>
-                    <Link to='/Projetos/Orgprojeto/2'>
-                        <div className='cardProjeto'>
-                            <div className='headerProjeto'><h3>Projeto TAL</h3></div>
-                            <div className='bodyProjeto'><p>A vida de redator freelancer não é fácil. Entre se manter atualizado em Marketing de Conteúdo e buscar novos clientes, também temos de dar muita atenção a uma habilidade básica: a escrita.</p>
-                                <p><strong>Criado por:</strong>Mateus Henrique de Oliveira</p></div>
-                            <div className='overProjeto'>
-
-                            </div>
-                        </div>
-                    </Link>
-
-
-                </div>
             </div>
         </div >
     )
